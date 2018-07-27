1a0e86c6549a2f347cb33b0a6765b63ce8e2e677
CurlyCuteLauren <lnziegele@gmail.com>
2018-07-27 16:24:29 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -1,4 +1,4 @@
-def Rainbow
+
 rainbow_colors = [ "yellow","default","light_cyan"]
 rainbow_colors[0]="red"
 rainbow_colors[1]="light_red"
@@ -6,4 +6,3 @@ rainbow_colors[2]= "light_yellow"
 rainbow_colors.push("green")
 rainbow_colors.push("blue")
 puts rainbow_colors
-end
